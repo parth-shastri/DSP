4cf60e4b0410e50a02115a4816cad59a9edb0cd7
Parth Shastri <shastriparth28@gmail.com>
2021-08-28 10:32:21 +0530

    Convolution of signals
    Convolution op

---
+++ CConv.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+x = [1, 2, 3, 4]
+h = [0, 1, 2]
+
+conv = np.convolve(x,h)
+print("linear convolution: ", conv)
+ccnlen = np.maximum(len(x), len(h))
+ccn = conv[:ccnlen]
+residual = conv[ccnlen:]
+residual = np.append(residual, [0 for i in range(ccnlen-len(residual))])
+
+for i in range(ccnlen):
+    ccn[i] = ccn[i] + residual[i]
+
+print(ccn)
+
+
+
+
+
